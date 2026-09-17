543c6ff5dfc94c28ffa2522463db6b8ca361dc98
susmitha-128 <120056912+susmitha-128@users.noreply.github.com>
2026-09-17 18:16:50 +0530

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -13,12 +13,7 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet:{
-          name: string;
-          value: string;
-          options: CookieOptions;
-        }[]
-         ) {
+        setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
